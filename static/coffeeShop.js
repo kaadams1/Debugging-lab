91917272e9a118042933b20faf216f54c65bda7c
Katherine Adams <kaadams1@gmail.com>
2022-06-03 16:05:05 -0700

    Debugging lab

---
+++ static/coffeeShop.js
@@ -0,0 +1,63 @@
+'use strict';
+
+let cart = {};
+let orderTotal = 0;
+
+// Display the shopping cart items in the right-hand column of the page.
+function displayCart(cartContents) {
+  let tableContents = '';
+
+  // Object.entries is like Python's dict.items --- it's a nice way to
+  // loop over keys and values. Here's how this would look in Python:
+  //
+  //   for item, price in cart_contents.items():
+  //       # etc.
+  for (const [item, price] of Object.entries(cartContents)) {
+    tableContents += `<tr><td>${item}</td><td>${price}</td></tr>`;
+  }
+
+  document.querySelector('#cart-items').innerHTML = tableContents;
+}
+
+// Display the order total using a dollar sign and two decimal places.
+function displayOrderTotal(total) {
+  document.querySelector('#cart-total').innerHTML = `${total.toFixed(2)}`;
+}
+
+
+
+const addButtons = document.querySelectorAll('.add-to-order');
+
+for (const button of addButtons) {
+  button.addEventListener('click', () => {
+    const item = button.id;
+
+  
+    // you could also hard code url to '/status?order=123'
+    const url = `/update-cart.json?item=${item}`;
+    // const formInputs = {
+    //   cart: document.querySelector('#cart-items').value,
+    //   total: document.querySelector('#cart-total').value,
+    // };
+    fetch(url, {
+      method: 'POST',
+      // body: JSON.stringify(formInputs),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result)
+        cart = result.cart;
+        orderTotal = result.total;
+
+        displayCart(cart);
+        displayOrderTotal(orderTotal);
+        
+      });
+
+  });
+}
+
+
